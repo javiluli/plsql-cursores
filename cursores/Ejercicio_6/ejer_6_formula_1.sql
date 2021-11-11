7b85687704971a4a7511eebf538e319d4db9c8c9
Javier Delgado <javiluli.dev@gmail.com>
2021-11-11 14:25:22 +0100

    Update ejer_6_formula_1.sql

--- cursores/Ejercicio_6/ejer_6_formula_1.sql
+++ cursores/Ejercicio_6/ejer_6_formula_1.sql
@@ -7,30 +7,30 @@ DECLARE
   CONTADORsalarios NUMBER:=0;
   AUX TEMPLE.NUMDE%TYPE;
   salario_aux TEMPLE.SALAR%TYPE;
-  
+
 BEGIN
   OPEN C1;
   FETCH C1 INTO LINEA_C1;
   WHILE C1%FOUND LOOP -- haya filas
-     AUX:= LINEA_C1.NUMDE;
-     WHILE C1%FOUND AND AUX=LINEA_C1.NUMDE LOOP -- haya filas y mismo departamento
-         contadorsalarios:=0;
-         WHILE C1%FOUND AND AUX=LINEA_C1.NUMDE AND contadorsalarios < 3  LOOP -- haya filas,mismo depto
-            salario_aux:= LINEA_C1.salar;                                         -- menos de 3  salarios
-           WHILE C1%FOUND AND AUX=LINEA_C1.NUMDE and salario_aux= LINEA_C1.salar  loop
-               DBMS_OUTPUT.PUT_LINE(LINEA_C1.NOMEM||'  '||AUX||'  '||LINEA_C1.salar);
-               FETCH C1 INTO LINEA_C1;
-               end loop;
-          -- fin mismo salario del mismo departamento .o distinto departamento, o ho hay datos    
-            contadorsalarios:=contadorsalarios+1;   
-         end loop;
-         -- forzar cambio de departamento o final de datos si se han tratado tres salarios
-         WHILE C1%FOUND AND AUX=LINEA_C1.NUMDE  loop
-         FETCH C1 INTO LINEA_C1;
-         end loop;
-     END LOOP;
-        -- cambio de departamento o fin de datos
+    AUX:= LINEA_C1.NUMDE;
+    WHILE C1%FOUND AND AUX=LINEA_C1.NUMDE LOOP -- haya filas y mismo departamento
+      contadorsalarios:=0;
+      WHILE C1%FOUND AND AUX=LINEA_C1.NUMDE AND contadorsalarios < 3 LOOP -- haya filas, mismo depto y menos de 3 salarios
+        salario_aux:= LINEA_C1.salar;
+        WHILE C1%FOUND AND AUX=LINEA_C1.NUMDE and salario_aux= LINEA_C1.salar  loop
+          DBMS_OUTPUT.PUT_LINE(LINEA_C1.NOMEM||'  '||AUX||'  '||LINEA_C1.salar);
+          FETCH C1 INTO LINEA_C1;
+        END LOOP;
+        -- fin mismo salario del mismo departamento .o distinto departamento, o ho hay datos
+        contadorsalarios:=contadorsalarios+1;
+      END LOOP;
+      -- forzar cambio de departamento o final de datos si se han tratado tres salarios
+      WHILE C1%FOUND AND AUX=LINEA_C1.NUMDE  loop
+        FETCH C1 INTO LINEA_C1;
+      END LOOP;
+    END LOOP;
+    -- cambio de departamento o fin de datos
   END LOOP;
-   --  fin de datos
+  -- fin de datos
   CLOSE C1;
 END;
